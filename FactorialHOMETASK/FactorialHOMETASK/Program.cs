05cb4c22ec91990f6cf644dfa1f006e9ff62c8b6
magomedfm <113282411+magomedfm@users.noreply.github.com>
2022-10-28 23:28:19 +0400

    Add files via upload

---
+++ FactorialHOMETASK/FactorialHOMETASK/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace FactorialHOMETASK
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 4;
+            int factorial = 1;
+            for (int x = 1; x <= a; x++)
+            {
+                factorial *= x;
+            }
+            Console.WriteLine(factorial);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ FactorialHOMETASK/README.md
@@ -0,0 +1,2 @@
+# FactorialHOMETASK
+# FactorialHOMETASK
